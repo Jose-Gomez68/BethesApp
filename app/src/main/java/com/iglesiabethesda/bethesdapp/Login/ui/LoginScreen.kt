060fc8e1457662cd2826ddc425f685790a7e2d57
joseGomez <jose_armando_za@hotmail.com>
2025-09-22 21:35:44 -0600

    feat:(Login): delete comment

--- app/src/main/java/com/iglesiabethesda/bethesdapp/Login/ui/LoginScreen.kt
+++ app/src/main/java/com/iglesiabethesda/bethesdapp/Login/ui/LoginScreen.kt
@@ -1,6 +1,5 @@
 package com.iglesiabethesda.bethesdapp.Login.ui
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
